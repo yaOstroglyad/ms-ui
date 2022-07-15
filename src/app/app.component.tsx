98b130d40039214c64bfc1a5fb1014f7da2d3291
vladyslav.tkach <vladyslav.tkach@gmail.com>
2022-07-15 12:34:16 +0300

    Improved styles

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
 import {AppRoutes} from './app.routes';
+
+const theme = createTheme();
 
 export const AppComponent: React.FC = () => (
   <BrowserRouter>
     <React.Suspense>
+      <ThemeProvider theme={theme}>
       <AppRoutes />
+      </ThemeProvider>
     </React.Suspense>
   </BrowserRouter>
 )
