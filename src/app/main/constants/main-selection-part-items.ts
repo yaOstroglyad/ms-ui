1dde628a97b06d337477060d9871f30a14d456a0
andreyostroglyad <andrey@triber.app>
2022-07-15 19:58:19 +0300

    - tabs added - container added

--- src/app/main/constants/main-selection-part-items.ts
+++ src/app/main/constants/main-selection-part-items.ts
@@ -1,12 +1,13 @@
 import {MainSelectionPartItem} from '../main-selection-part/main-selection-part.component';
 
 export const MAIN_SELECTION_PART_ITEMS: ReadonlyArray<MainSelectionPartItem> = [
-  {
-    label: 'System',
-    route: './system',
-  },
-  {
-    label: 'Network',
-    route: './network',
-  }
+  { label: 'System', route: './system', },
+  { label: 'Network', route: './network', },
+  { label: 'eNodeB', route: './eNodeB', },
+  { label: 'Serial', route: './serial', },
+  { label: 'Backhaul', route: './backhaul', },
+  { label: 'GPS', route: './gps', },
+  { label: 'Diagnostic', route: './diagnostic', },
+  { label: 'Logs', route: './logs', },
+  { label: 'Admin', route: './admin', },
 ];
--- src/app/main/main.component.tsx
+++ src/app/main/main.component.tsx
@@ -4,12 +4,15 @@ import './main.style.scss';
 import {MainSelectionPart} from './main-selection-part/main-selection-part.component';
 import {MAIN_SELECTION_PART_ITEMS} from './constants/main-selection-part-items';
 import {MainRoutes} from './main-routes';
+import {Container} from '@mui/material';
 
 export const Main: React.FC = () => (
   <>
-    <MainSelectionPart selectionItems={MAIN_SELECTION_PART_ITEMS}/>
-    <main className="main">
-      <MainRoutes />
-    </main>
+      <Container maxWidth="lg">
+        <MainSelectionPart selectionItems={MAIN_SELECTION_PART_ITEMS}/>
+        <main className="main">
+          <MainRoutes />
+        </main>
+      </Container>
   </>
 );
