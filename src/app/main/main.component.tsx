3beb4a4278082c4ddc8f65d37e12a7071b09de69
andreyostroglyad <andrey@triber.app>
2022-07-15 20:10:29 +0300

    - demo table added

--- src/app/main/main.component.tsx
+++ src/app/main/main.component.tsx
@@ -5,12 +5,14 @@ import {MainSelectionPart} from './main-selection-part/main-selection-part.compo
 import {MAIN_SELECTION_PART_ITEMS} from './constants/main-selection-part-items';
 import {MainRoutes} from './main-routes';
 import {Container} from '@mui/material';
+import DataGridDemo from '../_shared/components/table/table.component';
 
 export const Main: React.FC = () => (
   <>
       <Container maxWidth="lg">
         <MainSelectionPart selectionItems={MAIN_SELECTION_PART_ITEMS}/>
         <main className="main">
+            <DataGridDemo />
           <MainRoutes />
         </main>
       </Container>
