c3a679b4f8412c76bfc88126e8e45c634db089fd
vladyslav.tkach <vladyslav.tkach@gmail.com>
2022-07-18 14:31:53 +0300

    Reverted system component markup

--- src/app/system/system-general/system-general.component.tsx
+++ src/app/system/system-general/system-general.component.tsx
@@ -1,36 +1,13 @@
-import React, {useEffect} from 'react';
-import {useFetch} from '../../_shared/modules/http/hooks/use-fetch';
+import React from 'react';
 
-interface Post {
-  id: string;
-  title: string;
-  body: string;
-  userId: string;
-}
+import {Typography} from '@mui/material';
+import Box from '@mui/material/Box';
 
-export const SystemGeneral: React.FC = () => {
-  // const {isLoading, data} = useFetch<Post>({url: 'https://jsonplaceholder.typicode.com/posts/1', method: 'GET'})();
-  const {operation: createPost} = useFetch<Post>();
-  const s = () => {
-    createPost({
-      url: 'https://jsonplaceholder.typicode.com/posts',
-      method: 'POST',
-      headers: {'Content-type': 'application/json; charset=UTF-8'},
-      body: {
-        title: 'post title',
-        body: 'post description',
-        userId: 'userID',
-      },
-    });
-    console.log('Post created');
-  };
-  useEffect(() => {
+import DataGridDemo from '../../_shared/components/table/table.component';
 
-  }, []);
-  return (
-    <div>
-      <div>Testing http...</div>
-      <div><button onClick={s}>Create a new post</button></div>
-    </div>
-  );
-};
+export const SystemGeneral: React.FC = () => (
+  <Box sx={{display: 'flex', flexDirection: 'column', gap: '1.5rem'}}>
+    <Typography variant="h5">General with table :</Typography>
+    <DataGridDemo/>
+  </Box>
+);
