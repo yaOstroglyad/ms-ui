c2f62976d665b2f921c9a68f57cec4eb870836e0
vladyslav.tkach <vladyslav.tkach@gmail.com>
2022-07-20 20:37:46 +0300

    Added cache storage to auth provider

--- src/app/system/system-general/system-general.component.tsx
+++ src/app/system/system-general/system-general.component.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Typography} from '@mui/material';
 import Box from '@mui/material/Box';
 
 import DataGridDemo from '../../_shared/components/table/table.component';
+import {useHttp} from '../../_shared/modules/http/hooks/use-http';
 
-export const SystemGeneral: React.FC = () => (
-  <Box sx={{
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '1.5rem',
-  }}>
-    <Typography variant="h5">General with table :</Typography>
-    <DataGridDemo/>
-  </Box>
-);
+export const SystemGeneral: React.FC = () => {
+  const {operation: getSmth, data} = useHttp<{id: string;}>({url: 'https://jsonplaceholder.typicode.com/todos/1', method: 'GET'});
+  useEffect(() => {
+    getSmth({});
+  }, []);
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1.5rem',
+    }}>
+      <Typography variant="h5">General with table :</Typography>
+      <DataGridDemo/>
+    <div>{data?.id}</div>
+    </Box>
+  );
+}
