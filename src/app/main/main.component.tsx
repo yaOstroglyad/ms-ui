4ddeb572c4f244457972df7769ea3874ea4fa2b6
andreyostroglyad <andrey@triber.app>
2022-07-20 14:28:17 +0300

    - header added - removed some trash files

--- src/app/main/main.component.tsx
+++ src/app/main/main.component.tsx
@@ -10,10 +10,42 @@ import {SelectionPart} from '../_shared/components/selection-part/selection-part
 export const Main: React.FC = () => (
   <>
     <Container maxWidth="lg">
-      <SelectionPart selectionItems={MAIN_SELECTION_PART_ITEMS}/>
-      <main className="main">
-        <MainRoutes/>
-      </main>
+        <header className="container center main-bg">
+            <div className="logo">
+                Management System
+                <strong className="str">-UI</strong>
+            </div>
+            <ul className="widgets">
+                <li className="list">
+                    <div className="typing-indicator">
+                        <span style={{backgroundColor: "green"}}></span>
+                        <span style={{backgroundColor: "green"}}></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </li>
+                <li className="list">
+                    <div className="typing-indicator single-dot">
+                        <span style={{backgroundColor: "green"}}></span>
+                    </div>
+                    Power
+                </li>
+                <li className="list">
+                    <div className="typing-indicator single-dot">
+                        <span></span>
+                    </div>
+                    Access
+                </li>
+                <button>Logout</button>
+            </ul>
+        </header>
+        <div className="container main-bg" style={{margin: "140px 160px 30px 160px"}}>
+          <SelectionPart selectionItems={MAIN_SELECTION_PART_ITEMS}/>
+          <main className="main">
+            <MainRoutes/>
+          </main>
+        </div>
     </Container>
   </>
 );
